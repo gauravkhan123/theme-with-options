4130a8199aafa541efddab25a531dc2d7a05776c
gauravkhan123 <gauravkhan123@outlook.com>
2024-04-09 19:00:27 +0530

    split header and footer code

--- footer.php
+++ footer.php
@@ -11,26 +11,13 @@
 
 ?>
 
-	</div><!-- #content -->
+<footer>
+	<!-- Footer content here -->
+</footer>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ct-custom' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ct-custom' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ct-custom' ), 'ct-custom', '<a href="https://coalitiontechnologies.com/">Coalition Technologies</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -12,8 +12,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,38 +22,45 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ct-custom' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	<div class="top-header">
+		<div class="top-header-content">
+			<div class="call-us"><span>CALL US NOW!</span>
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				$phone = esc_html(get_option('phone'));
+				if (!empty($phone)) {
+					echo $phone = esc_html($phone);
+				} else {
+					echo '385.154.11.28.38';
+				} ?>
+			</div>
+			<div class="login-signup"><a href="#"><span>LOGIN</span></a><a href="#">SIGNUP</a></div>
+		</div>
+	</div>
+	<header>
+		<div class="header-content">
+			<div class="logo">
 				<?php
-			endif;
-			$ct_custom_description = get_bloginfo( 'description', 'display' );
-			if ( $ct_custom_description || is_customize_preview() ) :
+				$logo = esc_attr(get_option('logo'));
+				$site_url = site_url();
+				if (!empty($logo)) {
+					echo '<a href="' . $site_url . '"><img src="' . $logo . '" alt="Logo" width="100px;"></a>';
+				} else {
+					echo 'YOUR LOGO';
+				}
 				?>
-				<p class="site-description"><?php echo $ct_custom_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ct-custom' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+			</div>
+			<nav>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'main-menu',
+						'menu_class' => 'main-menu',
+						'container' => false,
+						'depth' => 2, // Allow 2 levels of submenus
+					)
+				);
+				?>
+			</nav>
+		</div>
+	</header>
+	<!-- ... -->
